882bced78579ae6e1c52db103ed672ceaf79f8f5
Alvaro <alvarojimenez95@hotmail.com>
2016-01-28 17:07:07 +0100

    28/01/2016

--- src/ejerciciosDelTema/PalabraLeida.java
+++ src/ejerciciosDelTema/PalabraLeida.java
@@ -4,8 +4,20 @@ public class PalabraLeida {
 	//atributos
 	private String valor;
 	
-	//metodo para calcular el numero de letras
-	public static int numeroLetras(){
-		return ;
+	//constructor
+	public PalabraLeida(String valor) {
+		this.valor = valor;
 	}
+	
+	//metodo para calcular el numero de letras
+	public int numeroLetras(){
+		return this.valor.length();
+	}
+	
+	//metodo para determinar si una palabra empieza por vocal
+	public boolean empiezaVocal(){
+		return this.valor.startsWith(valor);
+	}
+	
+	//
 }
