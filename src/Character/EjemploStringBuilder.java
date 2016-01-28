a868a0599566164763083c7b44b3630a5b8e5063
Alvaro <alvarojimenez95@hotmail.com>
2016-01-28 12:15:57 +0100

    28/01/2016

--- src/Character/EjemploStringBuilder.java
+++ src/Character/EjemploStringBuilder.java
@@ -1,14 +1,40 @@
 package Character;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class EjemploStringBuilder {
 
 	public static void main(String[] args) {
-		//leemos con Scanner las IPs
+		//ejecutamos cat log.txt | java Ejemplo
+		//leemos los datos con Scanner
+		//creamos un StringBuilder y añadimos
+		//IPs usando una expresion regular
 		Scanner in = new Scanner(System.in);
-		System.out.println("Introduce IPs válidas");
-
+		
+		//creamos un Set de String
+		//guarda objetos diferentes
+		Set<String> EjemploStringBuilder = new HashSet<String>();
+		
+		//creamos el patron de IPs
+		final String REGEX_IP = "((25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)\\.){3}"
+				+ "(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)";
+		
+		//leemos palabra a palabra
+		//preparamos una variable para almacenar in.next()
+		int contador = 0; //contamos las IPs que hay
+		String palabra = "";
+		while (in.hasNext()){
+			palabra = in.next();
+			if (palabra.matches(REGEX_IP)){
+				EjemploStringBuilder.add(palabra);
+			}
+		}
+		//cerrar el Scanner
+		//quitamos el ultimo caracter
+		in.close();
+		System.out.println(EjemploStringBuilder);
+		System.out.println("Numero de IPs: "+contador);
 	}
-
 }
