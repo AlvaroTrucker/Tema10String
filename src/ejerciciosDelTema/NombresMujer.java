5390141e24981dbb7e2a7389a939f81f805b9e45
Alvaro <alvarojimenez95@hotmail.com>
2016-02-03 14:41:08 +0100

    03/02/2016

--- src/ejerciciosDelTema/NombresMujer.java
+++ src/ejerciciosDelTema/NombresMujer.java
@@ -1,10 +1,44 @@
 package ejerciciosDelTema;
 
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class NombresMujer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//leemos con Scanner
+		Scanner in = new Scanner(System.in);
+		String palabras;
+		int contador = 0;
 
+		while (in.hasNextLine()){
+			palabras = in.nextLine();
+			contador++;
+		}
+		
+		//comprobaciones
+		System.out.println("Hay "+contador+" palabras en el fichero");
+	
+		//lista para los nombres que empiezan por A
+		List<String> palabrasComienzaPorA = new ArrayList<String>();
+		
+		//lista para los nombres que no acaban en vocal
+		List<String> palabrasAcabanEnConsonante = new ArrayList<String>();
+		
+		while (in.hasNextLine()){
+			palabras = in.nextLine();
+			if (palabras.toLowerCase().startsWith("a"))
+				palabrasComienzaPorA.add(palabras);
+			contador++;
+			if (palabras.toLowerCase().matches(".*[^aeiou]$"))
+				palabrasAcabanEnConsonante.add(palabras);
+		}
+		System.out.println(palabrasComienzaPorA);
+		System.out.println(palabrasAcabanEnConsonante);
+		
+	//metodo que comprueba las palabras con mayor tamaño
+	
+	//metodo que comprueba las palabras con menor tamaño
 	}
-
 }
--- src/ejerciciosDelTema/TestConstitucion.java
+++ src/ejerciciosDelTema/TestConstitucion.java
@@ -1,10 +1,30 @@
 package ejerciciosDelTema;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TestConstitucion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//leemos con Scanner
+		Scanner in = new Scanner(System.in);
+		
+		//lista para guardar las palabras
+		List<String> palabrasConstitucion = new ArrayList<String>();
+		
+		//StringBuilder para almacenar las palabras del ArrayList
+		StringBuilder sb = new StringBuilder();
+		
+		String palabra;
+		int contador = 0;
+		while(in.hasNext() && sb.length()<500){
+			palabra = in.nextLine();
+			palabrasConstitucion.add(palabra);
+			sb.append(palabra);
+			contador++;
+		}
+		System.out.println("Hay "+contador+" palabras en el fichero");
+		System.out.println();
 	}
-
 }
